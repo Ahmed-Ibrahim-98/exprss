43375895574d2b2a6d23670d18cdec9d01e269ab
Mousa <ahmedimosa677@gmail.com>
2025-05-25 00:22:15 +0300

    mongodb

--- app1.js
+++ app1.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express()
-const port =3030
+const port = 3030
+const mongoose = require('mongoose');
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.sendFile('./views/home.html', { root: __dirname })
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
+
+mongoose
+    .connect('mongodb+srv://<db_username>:ahmedmousa@cluster0.stq4muw.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+    app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+        })
+    })
+    .catch((err) => {
+         console.log(err)
+    })
+    
